648a2cd58411db6cb4ed4f157e1108a5f82ccf48
Andreas Hakansson <andreas@selfinflicted.org>
2011-07-25 10:13:55 +0200

    Started cleaning up the Unity bootstrapper

---
+++ src/Nancy.Bootstrappers.Unity.Tests/FakeUnityNancyBootstrapper.cs
@@ -0,0 +1,34 @@
+namespace Nancy.BootStrappers.Unity.Tests
+{
+    using Microsoft.Practices.Unity;
+    using Nancy.Bootstrappers.Unity;
+    using Nancy.Tests.Fakes;
+
+    public class FakeUnityNancyBootstrapper : UnityNancyBootstrapper
+    {
+        public bool ApplicationContainerConfigured { get; set; }
+
+        public IUnityContainer Container
+        {
+            get { return this.ApplicationContainer; }
+        }
+
+        public bool RequestContainerConfigured { get; set; }
+
+        protected override void InitialiseInternal(IUnityContainer container)
+        {
+            base.InitialiseInternal(container);
+
+            RequestContainerConfigured = true;
+
+            container.RegisterType<IFoo, Foo>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IDependency, Dependency>(new ContainerControlledLifetimeManager());
+        }
+
+        protected override void ConfigureApplicationContainer(IUnityContainer existingContainer)
+        {
+            ApplicationContainerConfigured = true;
+            base.ConfigureApplicationContainer(existingContainer);
+        }
+    }
+}
