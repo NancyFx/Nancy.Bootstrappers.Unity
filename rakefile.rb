d18fcc10f2db73d3d6e07fdff8616cd14987d176
Andreas Hakansson <andreas@selfinflicted.org>
2011-07-27 23:46:32 +0200

    Updated Nancy dependency to version 0.7.1 and bumped version in bootstrapper Rake

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ require 'albacore'
 require 'rake/clean'
 require 'rexml/document'
 
-NANCY_VERSION = "0.6.0"
+NANCY_VERSION = "0.7.1"
 OUTPUT = "build"
 CONFIGURATION = 'Release'
 SHARED_ASSEMBLY_INFO = 'dependencies/Nancy/src/SharedAssemblyInfo.cs'
@@ -28,7 +28,7 @@ assemblyinfo :version => [:clean] do |asm|
 	asm.company_name = "Nancy"
 	asm.product_name = "Nancy.Bootstrappers.Unity"
 	asm.title = "Nancy.Bootstrappers.Unity"
-	asm.description = "An Unity Bootstrapper for the Nancy web framework"
+	asm.description = "A Unity Bootstrapper for the Nancy web framework"
 	asm.copyright = "Copyright (C) Andreas Hakansson, Steven Robbins and contributors"
     asm.output_file = SHARED_ASSEMBLY_INFO
 end
