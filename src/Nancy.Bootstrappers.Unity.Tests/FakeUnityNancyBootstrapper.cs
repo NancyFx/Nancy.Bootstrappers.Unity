b7056793fa9af4d3506922222540d2fd9db93580
Andreas Hakansson <andreas@selfinflicted.org>
2012-06-26 08:01:11 +0200

    Updated FakeUnityNancyBootstrapper
    Added ctor overload that accepts a NancyInternalConfiguration
    instance that is returned by the InternalConfiguration property
    if set, else the base's configs are returned.

--- src/Nancy.Bootstrappers.Unity.Tests/FakeUnityNancyBootstrapper.cs
+++ src/Nancy.Bootstrappers.Unity.Tests/FakeUnityNancyBootstrapper.cs
@@ -1,29 +1,34 @@
 namespace Nancy.BootStrappers.Unity.Tests
 {
+    using Bootstrapper;
     using Microsoft.Practices.Unity;
     using Nancy.Bootstrappers.Unity;
     using Nancy.Tests.Fakes;
 
     public class FakeUnityNancyBootstrapper : UnityNancyBootstrapper
     {
+        private readonly NancyInternalConfiguration configuration;
         public bool ApplicationContainerConfigured { get; set; }
+        public bool RequestContainerConfigured { get; set; }
 
-        public IUnityContainer Container
+        public FakeUnityNancyBootstrapper()
+            : this(null)
         {
-            get { return this.ApplicationContainer; }
         }
 
-        public bool RequestContainerConfigured { get; set; }
+        public FakeUnityNancyBootstrapper(NancyInternalConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
 
         protected override void ApplicationStartup(IUnityContainer container, Bootstrapper.IPipelines pipelines)
         {
             RequestContainerConfigured = true;
         }
 
-        protected override IUnityContainer CreateRequestContainer()
+        public IUnityContainer Container
         {
-            this.RequestContainerConfigured = true;
-            return base.CreateRequestContainer();
+            get { return this.ApplicationContainer; }
         }
 
         protected override void ConfigureApplicationContainer(IUnityContainer existingContainer)
@@ -33,6 +38,17 @@ namespace Nancy.BootStrappers.Unity.Tests
 
             existingContainer.RegisterType<IFoo, Foo>(new ContainerControlledLifetimeManager());
             existingContainer.RegisterType<IDependency, Dependency>(new ContainerControlledLifetimeManager());
+        }
+
+        protected override IUnityContainer CreateRequestContainer()
+        {
+            this.RequestContainerConfigured = true;
+            return base.CreateRequestContainer();
+        }
+
+        protected override NancyInternalConfiguration InternalConfiguration
+        {
+            get { return this.configuration ?? base.InternalConfiguration; }
         }
     }
 }
