a5b30b903eeb63a3286cb10849fa2febfd017d25
Kristian Hellang <kristian@hellang.com>
2015-09-17 10:00:30 +0200

    Added failing tests

--- src/Nancy.Bootstrappers.Unity.Tests/FakeUnityNancyBootstrapper.cs
+++ src/Nancy.Bootstrappers.Unity.Tests/FakeUnityNancyBootstrapper.cs
@@ -1,32 +1,49 @@
 namespace Nancy.BootStrappers.Unity.Tests
 {
-    using System;
-    using System.Collections.Generic;
-
     using Bootstrapper;
     using Microsoft.Practices.Unity;
-    using Nancy.Bootstrappers.Unity;
+    using Bootstrappers.Unity;
     using Nancy.Tests.Fakes;
 
     public class FakeUnityNancyBootstrapper : UnityNancyBootstrapper
     {
+        private readonly IUnityContainer container;
+
         private readonly NancyInternalConfiguration configuration;
-        public bool ApplicationContainerConfigured { get; set; }
-        public bool RequestContainerConfigured { get; set; }
 
         public FakeUnityNancyBootstrapper()
-            : this(null)
+            : this(null, null)
         {
         }
 
         public FakeUnityNancyBootstrapper(NancyInternalConfiguration configuration)
+            : this(configuration, null)
         {
-            this.configuration = configuration;
         }
 
-        protected override void ApplicationStartup(IUnityContainer container, Bootstrapper.IPipelines pipelines)
+        public FakeUnityNancyBootstrapper(IUnityContainer container)
+            : this(null, container)
         {
-            RequestContainerConfigured = true;
+        }
+
+        public FakeUnityNancyBootstrapper(NancyInternalConfiguration configuration, IUnityContainer container)
+        {
+            this.configuration = configuration;
+            this.container = container;
+        }
+
+        public bool ApplicationContainerConfigured { get; private set; }
+
+        public bool RequestContainerConfigured { get; private set; }
+
+        protected override IUnityContainer GetApplicationContainer()
+        {
+            return this.container ?? base.GetApplicationContainer();
+        }
+
+        protected override void ApplicationStartup(IUnityContainer container, IPipelines pipelines)
+        {
+            this.RequestContainerConfigured = true;
         }
 
         public IUnityContainer Container
@@ -36,11 +53,16 @@ namespace Nancy.BootStrappers.Unity.Tests
 
         protected override void ConfigureApplicationContainer(IUnityContainer existingContainer)
         {
-            ApplicationContainerConfigured = true;
+            this.ApplicationContainerConfigured = true;
             base.ConfigureApplicationContainer(existingContainer);
 
             existingContainer.RegisterType<IFoo, Foo>(new ContainerControlledLifetimeManager());
             existingContainer.RegisterType<IDependency, Dependency>(new ContainerControlledLifetimeManager());
+        }
+
+        public IUnityContainer CreateRequestContainer()
+        {
+            return this.CreateRequestContainer(new NancyContext());
         }
 
         protected override IUnityContainer CreateRequestContainer(NancyContext context)
